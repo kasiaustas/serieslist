ccdd6ac06b38c90844815839aedf906590329bd5
kasiaustas <kasiaustas@mail.ru>
2020-03-30 00:41:34 +0300

    test3

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/serieslist'));
 
 app.listen(process.env.PORT || 8080);
 
 //PathLocationStrategy
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/serieslist/index.html'));
 })
 
 console.log('Console listening!')
